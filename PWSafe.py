e796575675bde70df198436c73e263eb79bb661c
Jay Hayward <jay@jayhayward.com>
2025-06-26 09:25:25 -0600

    Create base python file
    Define TODO

---
+++ PWSafe.py
@@ -0,0 +1,11 @@
+# Jay Hayward 2025
+
+# TODO:
+# parse input string from argparse
+# parse encoding method from argparse
+# provide default perams from argparse
+# Convert method to b64 encoding (method % 64)
+
+# Create a UI with 2 text boxes from in_str and method
+# Accept arguments from text boxes
+# Create an executable to make this run-able from any source.

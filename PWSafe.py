6ea959c37e8187c08b0c476d8c66efe1624ad521
Jay Hayward <jay@jayhayward.com>
2025-06-26 10:49:20 -0600

    Update main script to call helper script

--- PWSafe.py
+++ PWSafe.py
@@ -1,11 +1,23 @@
 # Jay Hayward 2025
 
 # TODO:
-# parse input string from argparse
-# parse encoding method from argparse
-# provide default perams from argparse
-# Convert method to b64 encoding (method % 64)
+# Convert algorithm to b64 encoding (method % 64)
 
 # Create a UI with 2 text boxes from in_str and method
 # Accept arguments from text boxes
 # Create an executable to make this run-able from any source.
+
+import InParser  # my custom parser script
+
+def main():
+    argv = InParser.CustomParser()
+    print(f'Printing input string: "{argv.input_str}"')
+
+    if(argv.alg == 'base64'):
+        # do stuff
+        return()
+
+
+
+if __name__ == '__main__':
+    main()
